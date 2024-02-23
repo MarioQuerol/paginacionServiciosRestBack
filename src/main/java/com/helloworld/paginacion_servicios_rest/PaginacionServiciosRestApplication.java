2027f0a8e19bece62bfba3f31a4245d1022dd481
Mario Querol <mario@devanddel.com>
2024-02-23 11:54:24 +0100

    First commit v2

---
+++ src/main/java/com/helloworld/paginacion_servicios_rest/PaginacionServiciosRestApplication.java
@@ -0,0 +1,13 @@
+package com.helloworld.paginacion_servicios_rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaginacionServiciosRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaginacionServiciosRestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/helloworld/paginacion_servicios_rest/PaginacionServiciosRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.helloworld.paginacion_servicios_rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaginacionServiciosRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
